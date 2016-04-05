fdfa143e6081763fb67dbfe3a6fabf27cabe7d7e
ZhaoXiangYi <Xiangyizhao1991@gmail.com>
2016-04-05 22:41:56 +0800

    Summary: Update loginfo in terminal.

--- auto_backup.py
+++ auto_backup.py
@@ -25,17 +25,17 @@ class Auto_backup(object):
 
     def __init__(self):
         #super(Result, self).__init__()
-        sep = os.path.sep
+        print "[INFO]Current Time:",time.strftime('%Y-%m-%d,%H:%M',time.localtime(time.time()))
         self.logfolder = "logs_" + time.strftime('%Y_%m_%d_%H_%M_%S',time.localtime(time.time()))
         self.backupfolder = os.getcwd() + sep + backupfolder
         print os.getcwd()
-        logpath = os.getcwd() + "\logs_" + time.strftime('%Y_%m_%d_%H_%M',time.localtime(time.time()))
+        logpath = os.getcwd() + sep + "logs_" + time.strftime('%Y_%m_%d_%H_%M',time.localtime(time.time()))
         print logpath
         os.mkdir(self.logfolder)
         if os.path.isdir(backupfolder):
-            print "backup folder has already exists."
+            print "[DEBUG]backup folder has already exists."
         else:
-            print "make backup folder Graphics."
+            print "[INFO]make backup folder %s." % (backupfolder)
             os.mkdir(self.backupfolder)
 
     def remote_dir(self, url, dir):
@@ -47,14 +47,14 @@ class Auto_backup(object):
         sub_localpath = localhome + dir
         print sub_localpath
         if os.path.isdir(sub_localpath):
-            print "%s folder has already exists." % (sub_localpath)
+            print "[DEBUG]%s folder has already exists." % (sub_localpath)
         else:
-            print "make local tree folder %s." % (sub_localpath)
+            print "[INFO]make local tree folder %s." % (sub_localpath)
             os.mkdir(sub_localpath)
         return sub_localpath
 
     def craw_dir(self, url=home, localpath=backuppath, tmplog="tmp.log"):
-        tmplogs=self.logfolder + '/' + tmplog
+        tmplogs=self.logfolder + sep + tmplog
         f = open(tmplogs,'w+')
         m = urllib2.urlopen(url).read()
         f.write(m)
@@ -67,14 +67,14 @@ class Auto_backup(object):
                 filematch = file_flag.search(line)
                 if dirmatch:
                     if dirmatch.groups()[1].find('.')>=0:
-                        print "Found new file %s, but it could not download now." % (dirmatch.groups()[1])
+                        print "[INFO]Found new file %s, but it could not download now." % (dirmatch.groups()[1])
                         logging.debug("Found new file %s, but it could not download now." % (dirmatch.groups()[1]))
                         pass
                     else:
                         dir_name=dirmatch.groups()[1]
                         dir_date=dirmatch.groups()[2].split()[0]
                         dir_time=dirmatch.groups()[2].split()[1]
-                        print "Current Time:",time.strftime('%Y-%m-%d,%H:%M',time.localtime(time.time())),'Found directory:',dir_name
+                        print "[INFO]Found directory:",dir_name
                         print dir_date, dir_time
                         sub_url=self.remote_dir(url, dir_name)
                         sub_localpath=self.local_tree(localpath, dir_name)
@@ -86,7 +86,7 @@ class Auto_backup(object):
                         file_name=filematch.groups()[1]
                         file_date=filematch.groups()[2].split()[0]
                         file_time=filematch.groups()[2].split()[1]
-                        print 'Found file:',file_name
+                        print '[INFO]Found file:',file_name
                         print file_date, file_time
                         file_time = file_date + ' ' + file_time
                         sub_url=self.remote_dir(url, file_name)
@@ -97,39 +97,36 @@ class Auto_backup(object):
                             self.download_file(sub_url, file_path)
                 else:
                     pass
-        print "****************************************************************************"
-        print "Update local backup dir finished."
-        print "****************************************************************************"
 
     def check_localfile(self, filepath):
         if os.path.exists(filepath):
             if os.path.isdir(filepath):
-                print "new files comming! Remove the current file directory %s." % (filepath)
+                print "[DEBUG]new files comming! Remove the current file directory %s." % (filepath)
                 os.rmdir(filepath)
                 logging.debug("Remove the temporary file directory %s, start to download." % (filepath))
                 return False
             else:
-                print "%s is exists in local path." % (filepath)
+                print "[INFO]%s is exists in local path." % (filepath)
                 return True
         else:
-            print "%s is not exists in local path, start to download." % (filepath)
+            print "[INFO]%s is not exists in local path, start to download." % (filepath)
             return False
 
     def check_date(self, sub_url, file_path, file_time):
         file_time=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.mktime(time.strptime(file_time, '%d-%b-%Y %H:%M'))))
-        print "remote file modify time is:%s" % (file_time)
+        print "[INFO]remote file modify time is:%s" % (file_time)
         cacul_time = (datetime.datetime.now() - datetime.timedelta(days = cicle))
         reference_time = cacul_time.strftime('%Y-%m-%d %H:%M:%S')
-        print "yesterday reference time is:%s" % (reference_time)
+        print "[INFO]yesterday reference time is:%s" % (reference_time)
         if self.compare_time(reference_time, file_time):
-            print "%s did not update in artifactory." % (file_path)
+            print "[INFO]%s did not update in server." % (file_path)
         else:
-            print "%s is updated in artifactory,so download it." % (file_path)
+            print "[INFO]%s is updated in server,so download it." % (file_path)
             self.download_file(sub_url, file_path)
 
     def download_file(self, sub_url, file_path):
         print "****************************************************************************"
-        print "Start download %s" % (sub_url)
+        print "[INFO]Start download %s" % (sub_url)
         try:
             content=urllib2.urlopen(sub_url).read()
             with open(file_path,'wb') as code:
@@ -153,7 +150,7 @@ class Auto_backup(object):
             return False
 
     def clean_tmplog(self):
-        print "Clean the temporary log folder."
+        print "[INFO]Clean the temporary log folder."
         __import__('shutil').rmtree(self.logfolder)
 
 backup = Auto_backup()
@@ -162,6 +159,10 @@ def perform(inc):
     s.enter(inc,0,perform,(inc,))
     backup.craw_dir(home, backuppath, "tmp.log")
     backup.clean_tmplog()
+    print "[INFO]Current Time:",time.strftime('%Y-%m-%d,%H:%M',time.localtime(time.time()))
+    print "****************************************************************************"
+    print "Update local backup dir finished.waiting for next circle to backup."
+    print "****************************************************************************"
 
 def mymain(inc=86400*cicle):
     s.enter(0,0,perform,(inc,))
